65d41bfb25fc28dec7ec36c36b6dc2c5f9568d08
perf1lev <perfilevivan72@gmail.com>
2024-12-05 10:30:55 +0300

    Update task3.c

--- 3824Б1ФИ3/24 Перфильев Иван Павлович/task3.c
+++ 3824Б1ФИ3/24 Перфильев Иван Павлович/task3.c
@@ -1,7 +1,12 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b)
-{
-  if (a > b){printf("%lf", a);}
-  else (a < b){printf("%lf", b);}
-  return 0.0;
+
+double max_of_two_numbers(double a, double b) {
+    if (a > b) {
+        printf("%lf\n", a); 
+    }
+    else  {
+        printf("%lf\n", b); 
+    }
+        return 0.0;
+    
 }
